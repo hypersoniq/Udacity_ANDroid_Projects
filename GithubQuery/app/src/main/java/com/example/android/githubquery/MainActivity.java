a82b556c4be5bff7d4ee88a1bb4aaf48cf07dd51
John Close <hypersoniq@comcast.net>
2018-10-17 01:45:48 -0400

    Continue to refine the github search app

--- GithubQuery/app/src/main/java/com/example/android/githubquery/MainActivity.java
+++ GithubQuery/app/src/main/java/com/example/android/githubquery/MainActivity.java
@@ -4,15 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.example.android.githubquery.utilities.NetworkUtils;
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    // COMPLETED (26) Create an EditText variable called mSearchBoxEditText
     private EditText mSearchBoxEditText;
-
-    // COMPLETED (27) Create a TextView variable called mUrlDisplayTextView
     private TextView mUrlDisplayTextView;
-    // COMPLETED (28) Create a TextView variable called mSearchResultsTextView
     private TextView mSearchResultsTextView;
 
     @Override
@@ -20,12 +21,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // COMPLETED (29) Use findViewById to get a reference to mSearchBoxEditText
         mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
-
-        // COMPLETED (30) Use findViewById to get a reference to mUrlDisplayTextView
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
-        // COMPLETED (31) Use findViewById to get a reference to mSearchResultsTextView
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+    }
+
+    /**
+     * This method retrieves the search text from the EditText, constructs
+     * the URL (using {@link NetworkUtils}) for the github repository you'd like to find, displays
+     * that URL in a TextView, and finally fires off an AsyncTask to perform the GET request using
+     * our (not yet created) {@link GithubQueryTask}
+     */
+    private void makeGithubSearchQuery() {
+        String githubQuery = mSearchBoxEditText.getText().toString();
+        URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
+        mUrlDisplayTextView.setText(githubSearchUrl.toString());
+        String githubSearchResults = null;
+        try {
+            githubSearchResults = NetworkUtils.getResponseFromHttpUrl(githubSearchUrl);
+            mSearchResultsTextView.setText(githubSearchResults);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // COMPLETED (8) Override onCreateOptionsMenu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            makeGithubSearchQuery();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
